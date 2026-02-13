89d35a591bd282f3e67274c595e569a41553fa8f
Ihor Myronishyn <myronishyn.ihor@icloud.com>
2026-02-13 20:19:21 +0200

    Initial Commit

---
+++ Posts/PostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsApp.swift
+//  Posts
+//
+//  Created by Ihor Myronishyn on 13.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
